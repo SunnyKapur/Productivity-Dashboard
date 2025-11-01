5b2794ee7301fd86b7882c6ddb7a1598dd1cbf10
SunnyKapur <kumarsunnykumar678@gmail.com>
2025-11-01 16:30:01 +0530

    Dhasboard list added

---
+++ script.js
@@ -0,0 +1,20 @@
+function openFeatures() {
+  var allElems = document.querySelectorAll(".elem");
+  var fullElemPage = document.querySelectorAll(".fullElem");
+  var fullElemPageBackBtn = document.querySelectorAll(".fullElem .back");
+
+  allElems.forEach(function (elem) {
+    //   console.log(elem.id);
+
+    elem.addEventListener("click", function () {
+      fullElemPage[elem.id].style.display = "block";
+    });
+  });
+
+  fullElemPageBackBtn.forEach(function (back) {
+    back.addEventListener("click", function () {
+      // console.log(back.id)
+      fullElemPage[back.id].style.display = "none";
+    });
+  });
+}
