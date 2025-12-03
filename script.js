5d04666ee06d2471d10663558f48232a306da45e
SunnyKapur <kumarsunnykumar678@gmail.com>
2025-12-03 23:08:43 +0530

    daily planner page in proccess

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+// 3:09 min se start karna hai
+
 function openFeatures() {
   var allElems = document.querySelectorAll(".elem");
   var fullElemPage = document.querySelectorAll(".fullElem");
@@ -18,4 +21,95 @@ function openFeatures() {
     });
   });
 }
-openFeatures()
+// openFeatures();
+
+function todoList() {
+
+  var currentTask = [];
+
+  if (localStorage.getItem("currentTask")) {
+    currentTask = JSON.parse(localStorage.getItem("currentTask"));
+  } else {
+    console.log("Task list is empty");
+  }
+
+  function renderTask() {
+    let allTask = document.querySelector(".allTask");
+    
+    let sum = "";
+    
+    currentTask.forEach(function (elem, idx) {
+      sum += `<div class="task">
+      <h5>${elem.task} <span class=${elem.imp}>imp</span> </h5>
+      <button id=${idx}>Mark as Completed</button>
+      </div>`;
+    });
+    
+    allTask.innerHTML = sum;
+    
+    localStorage.setItem("currentTask", JSON.stringify(currentTask));
+    
+    document.querySelectorAll(".task button").forEach(function (btn) {
+      btn.addEventListener("click", function () {
+        currentTask.splice(btn.id, 1);
+        renderTask();
+      });
+    });
+  }
+  
+  renderTask();
+  
+  let form = document.querySelector(".addTask form");
+  let taskInput = document.querySelector(".addTask form input");
+  let taskDetailsInput = document.querySelector(".addTask form textarea");
+  let taskCheckbox = document.querySelector(".addTask form #check");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    currentTask.push({
+      task: taskInput.value,
+      details: taskDetailsInput.value,
+      imp: taskCheckbox.checked,
+    });
+
+    renderTask();
+
+    taskInput.value = "";
+    taskDetailsInput.value = "";
+    taskCheckbox.checked = false;
+  });
+}
+
+// todoList();
+
+var dayPlanData = JSON.parse(localStorage.getItem('dayPlanData')) || {}
+var dayPlanner = document.querySelector('.day-planner')
+
+var hours = Array.from({length:18},(elem, idx)=>`${6+idx}:00 - ${7+idx}:00`)
+
+var dayPlannerInput = document.querySelectorAll('.day-planner input')
+  
+let wholeDaySum = '';
+hours.forEach(function(elem,idx){
+  
+  console.log(dayPlanData)
+  wholeDaySum = wholeDaySum + `<div class="day-planner-time">
+                    <p>${elem}</p>
+                    <input id=${idx} type="text" placeholder="...">
+                </div>`
+})
+
+dayPlanner.innerHTML = wholeDaySum
+console.log();
+
+
+dayPlannerInput.forEach(function(elem){
+  elem.addEventListener('input',function(){
+    dayPlanData[elem.id] = elem.value
+
+    localStorage.setItem('dayPlanData', JSON.stringify(dayPlanData))
+  })
+})
+
+console.log()
